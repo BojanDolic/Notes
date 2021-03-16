9da390178d939cbe0d6c1c638f3efd28d76f73c4
Bojan Dolic <63774884+BojanDolic@users.noreply.github.com>
2021-03-17 00:28:48 +0100

    Created project and Note entity

---
+++ app/src/main/java/com/dolic/kotlinnotesapp/entities/Note.kt
@@ -0,0 +1,15 @@
+package com.dolic.kotlinnotesapp.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Date
+
+@Entity(tableName = "notes_table")
+data class Note(
+    @PrimaryKey val id: Long = 0,
+    @ColumnInfo(name = "note_title") val noteTitle: String = "",
+    @ColumnInfo(name = "note_desc") val noteDesc: String = "",
+    @ColumnInfo(name = "date_created") val dateCreated: Long = 0,
+    @ColumnInfo(name = "note_tag") val noteTag: String = ""
+)
